bf71bd19256103389e2018ed11052f8ce622260c
lzting <f74111217@gs.ncku.edu.tw>
2024-01-03 01:02:07 +0800

    Merge branch 'main' into gameControl

--- Ghost4Control.v
+++ Ghost4Control.v
@@ -9,58 +9,58 @@ module Ghost4Control #(
 (
     input clk,
     input reset,
-    inout reg [$clog2(`WIDTH) - 1:0] x,
-    inout reg [$clog2(`HEIGHT) - 1:0] y,
+	 input  [$clog2(`WIDTH) - 1:0] x,
+    input  [$clog2(`HEIGHT) - 1:0] y,
+    output reg [$clog2(`WIDTH) - 1:0] next_x,
+    output reg [$clog2(`HEIGHT) - 1:0] next_y,
     inout reg [1:0] ghost_direction,
-	 input [`width_log2 - 1:0] player_x,
-	 input [`width_log2 - 1:0] player_y,
 	 input [`tile_row_num * `tile_col_num - 1:0] tilemap_walls
 );
 
     always @(posedge clk or negedge reset) begin
         if (!reset) begin
-            x <= 20;
-            y <= 160;
+            next_x <= 20;
+            next_y <= 160;
 				ghost_direction <= `dir_right;
         end
         else begin
 				if( (x == 140 && y == 160) ||(x == 200 && y == 240) || (x == 280 && y == 280) || (x == 200 && y == 320) || (x == 360 && y == 380) || (x == 200 && y == 60) || (x == 280 && y == 20) || (x == 140 && y == 120))begin
-					y <= y + speed;
-						x <= x;
+					next_y <= y + speed;
+						next_x <= x;
 					ghost_direction <= `dir_down;
 				end
 				else if((x == 20 && y == 160) || (x == 140 && y == 240) || (x == 200 && y == 280) || (x == 200 && y == 380) || (x == 360 && y == 440)||(x == 480 && y == 60)) begin
-					y <= y;
-					x <= x + speed;
+					next_y <= y;
+					next_x <= x + speed;
 					ghost_direction <= `dir_right;
 				end
 				else if((x == 480 && y == 380) || (x == 600 && y == 60) || (x == 600 && y == 440))begin
-					y <= y - speed;
-					x <= x;
+					next_y <= y - speed;
+					next_x <= x;
 					ghost_direction <= `dir_up;
 				end
 				else if((x == 280 && y == 320) || (x == 600 && y == 380) || (x == 600 && y == 20) || (x == 280 && y == 60) || (x == 200 && y == 120))begin
-					y <= y;
-					x <= x - speed;
+					next_y <= y;
+					next_x <= x - speed;
 					ghost_direction <= `dir_left;
 				end
 				else begin
 					case(ghost_direction)
 						`dir_up:begin
-							y <= y - speed;
-							x <= x;
+							next_y <= y - speed;
+							next_x <= x;
 						end
 						`dir_down:begin
-							y <= y + speed;
-							x <= x;
+							next_y <= y + speed;
+							next_x <= x;
 						end
 						`dir_left:begin
-							y <= y;
-							x <= x - speed;
+							next_y <= y;
+							next_x <= x - speed;
 						end
 						`dir_right:begin
-							y <= y;
-							x <= x + speed;
+							next_y <= y;
+							next_x <= x + speed;
 						end
 					
 					endcase
--- PlayerControl.v
+++ PlayerControl.v
@@ -12,7 +12,7 @@ module PlayerControl #(
     inout reg [$clog2(`HEIGHT) - 1:0] y,
     inout reg [1:0] player_direction,
 	input [`tile_row_num * `tile_col_num - 1:0] tilemap_walls,
-	inout reg [`tile_row_num * `tile_col_num - 1:0] tilemap_dots
+	input [`tile_row_num * `tile_col_num - 1:0] tilemap_dots
 );
 
 	wire [20:0] tile_idx;
@@ -61,11 +61,11 @@ module PlayerControl #(
 				// tilemap_dots <= 768'b000000000000000000000000000000000111111111111100001111111111111001000001000001000010000010000010010000010000010000100000100000100111111111111111111111111111111001000001001000000000010010000010010000010010000000000100100000100111111100111110011111001111111000000001000000100100000010000000000000010011111111111100100000000000000100100000000001001000000000000001111011111111011110000000000000010010000000000100100000000000000100111111111111001000000000000001001000000000010010000000011111111111111001111111111111100100000100000010010000001000001001111001111111111111111110011110000010010010000000000100100100000000100100100000000001001001000001111111001111100111110011111110010000000000001001000000000000100111111111111111111111111111111000000000000000000000000000000000;
         end
         else begin
-				if(tilemap_dots[tile_idx] == 1)
-				begin
-					// score <= score + 1;
-					tilemap_dots[tile_idx] <= 0;
-				end
+				// if(tilemap_dots[tile_idx] == 1)
+				// begin
+				// 	// score <= score + 1;
+				// 	tilemap_dots[tile_idx] <= 0;
+				// end
             if(!w) begin
 					if (tilemap_walls[tile_idx - `tile_col_num] == 1) begin
 						cur_dir <= player_direction;
@@ -75,6 +75,7 @@ module PlayerControl #(
 					else begin
 						cur_dir <= `dir_up;
 						player_direction <= `dir_up;
+						x <= x;
 						y <= y - speed;
 					end
 				end
@@ -87,6 +88,7 @@ module PlayerControl #(
 					else begin
 						cur_dir <= `dir_down;
 						player_direction <= `dir_down;
+						x <= x;
 						y <= y + speed;
 					end
 					end
@@ -100,6 +102,7 @@ module PlayerControl #(
 						cur_dir <= `dir_left;
 						player_direction <= `dir_left;
 						x <= x - speed;
+						y <= y;
 					end
 					end
 				else if(!d) begin
@@ -112,6 +115,7 @@ module PlayerControl #(
 						cur_dir <= `dir_right;
 						player_direction <= `dir_right;
 						x <= x + speed;
+						y <= y;
 					end
 				end
 				else begin
