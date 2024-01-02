56f012d351cefd8dfd96217f5823d533a56b9b3c
Bennywang1007 <bennywang1007@gmail.com>
2024-01-02 22:45:25 +0800

    temporary work

--- PacMan.v
+++ PacMan.v
@@ -25,11 +25,16 @@ module PacMan(
     wire [`width_log2 - 1:0] x;
     wire [`height_log2 - 1:0] y;
 
-    VGAStateMachine vga_sm(clk_25MHz, reset, x, y, hstate, vstate);
+    VGAStateMachine vga_sm(clk_25MHz, x, y, hstate, vstate);
 
     wire [`tile_row_num * `tile_col_num - 1:0] tilemap_walls;
-    wire [`tile_row_num * `tile_col_num - 1:0] tilemap_dots;
+    reg [`tile_row_num * `tile_col_num - 1:0] tilemap_dots;
     reg [`tile_row_num * `tile_col_num - 1:0] tilemap_big_dots;
+
+    
+    wire [`tile_row_num * `tile_col_num - 1:0] init_walls;
+    wire [`tile_row_num * `tile_col_num - 1:0] init_dots;
+    wire [`tile_row_num * `tile_col_num - 1:0] init_big_dots;
 
     wire [`width_log2 - 1:0] player_x;
     wire [`height_log2 - 1:0] player_y;
@@ -55,44 +60,42 @@ module PacMan(
 
     wire [`width_log2 - 1:0] ghost4_next_x;
     wire [`height_log2 - 1:0] ghost4_next_y;
-	wire [`width_log2 - 1:0] ghost4_direction;
+    wire [`width_log2 - 1:0] ghost4_direction;
     reg [`width_log2 - 1:0] ghost4_x;
     reg [`height_log2 - 1:0] ghost4_y;
-	 
-	reg [255:0] score;// TODO
+
+    reg [25:0] score;// TODO
     reg [8:0] ateDots;
     reg [2:0] game_state = `GAME_STATE_PLAYING;
     reg [3:0] power_count_down;
 
     integer i;
     integer j;
-	
+
 
     // TODO : initialize tilemap
-	 initial
-	 begin
-//		 $readmemb("./maps/walls.txt",tilemap_walls);
-//		 $readmemb("./maps/dots.txt",tilemap_dots);
-//		 $readmemb("./maps/big_dots.txt",tilemap_big_dots);
-		 
-		 
-//		 player_x = `PLAYER_SPAWN_POINT_X;
-//		 player_y = `PLAYER_SPAWN_POINT_Y;
-//		 ghost1_x = `GHOST_SPAWN_POINT_X;
-//		 ghost1_y = `GHOST_SPAWN_POINT_Y;
-//		 ghost2_x = `GHOST_SPAWN_POINT_X;
-//		 ghost2_y = `GHOST_SPAWN_POINT_Y;
-//		 ghost3_x = `GHOST_SPAWN_POINT_X;
-//		 ghost3_y = `GHOST_SPAWN_POINT_Y;
-//		 ghost4_x = `GHOST_SPAWN_POINT_X;
-//		 ghost4_y = `GHOST_SPAWN_POINT_Y;
-	 end
+    // initial begin
+    //     $readmemb("./maps/walls.txt",tilemap_walls);
+    //     $readmemb("./maps/dots.txt",tilemap_dots);
+    //     $readmemb("./maps/big_dots.txt",tilemap_big_dots);
 
-		assign tilemap_walls = 768'b111111111111111111111111111111111000000000000011110000000000000110111110111110111101111101111101101111101111101111011111011111011000000000000000000000000000000110111110110111111111101101111101101111101101111111111011011111011000000011000001100000110000000111111110111111011011111101111111111111101100000000000011011111111111111011011111111110110111111110000000000100000000100000000001111111101101111111111011011111111111111011000000000000110111111111111110110111111111101101111111100000000000000110000000000000011011111011111101101111110111110110000110000000000000000001100001111101101101111111111011011011111111011011011111111110110110111110000000110000011000001100000001101111111111110110111111111111011000000000000000000000000000000111111111111111111111111111111111;
-	 // clk for char update
+    //     player_x = `PLAYER_SPAWN_POINT_X;
+    //     player_y = `PLAYER_SPAWN_POINT_Y;
+    //     ghost1_x = `GHOST_SPAWN_POINT_X;
+    //     ghost1_y = `GHOST_SPAWN_POINT_Y;
+    //     ghost2_x = `GHOST_SPAWN_POINT_X;
+    //     ghost2_y = `GHOST_SPAWN_POINT_Y;
+    //     ghost3_x = `GHOST_SPAWN_POINT_X;
+    //     ghost3_y = `GHOST_SPAWN_POINT_Y;
+    //     ghost4_x = `GHOST_SPAWN_POINT_X;
+    //     ghost4_y = `GHOST_SPAWN_POINT_Y;
+    // end
+
+    assign tilemap_walls = 768'b111111111111111111111111111111111000000000000011110000000000000110111110111110111101111101111101101111101111101111011111011111011000000000000000000000000000000110111110110111111111101101111101101111101101111111111011011111011000000011000001100000110000000111111110111111011011111101111111111111101100000000000011011111111111111011011111111110110111111110000000000100000000100000000001111111101101111111111011011111111111111011000000000000110111111111111110110111111111101101111111100000000000000110000000000000011011111011111101101111110111110110000110000000000000000001100001111101101101111111111011011011111111011011011111111110110110111110000000110000011000001100000001101111111111110110111111111111011000000000000000000000000000000111111111111111111111111111111111;
+    // clk for char update
 
     wire clk_100Hz;
-    FrequencyDivider #(.target_frequency(5)) char_clk_divider(clk_50MHz, reset, clk_100Hz);
+    FrequencyDivider #(.target_frequency(5)) char_clk_divider(clk_50MHz, 1, clk_100Hz);
 
     // TODO : add ghosts' data and behaviors
 
@@ -103,26 +106,35 @@ module PacMan(
         else vsync <= 1'b1;
     end
 
-//  display debug
+    // InitMap init_map(
+    //     .reset(clk_50MHz),
+    //     // .tilemap_walls(init_walls),
+    //     // .tilemap_dots(init_dots),
+    //     // .tilemap_big_dots(init_big_dots)
+    //     .tilemap_dots(tilemap_dots),
+    //     .tilemap_big_dots(tilemap_big_dots)
+    // );
+
+// display debug
 //    always @(posedge clk_25MHz or negedge reset) begin
 
-//        ghost1_x <= 50;
-//        ghost1_y <= 100;
-//        ghost1_direction <= `dir_up;
-//
-//        ghost2_x <= 160;
-//        ghost2_y <= 70;
-//        ghost2_direction <= `dir_left;
-//
-//        ghost3_x <= 80;
-//        ghost3_y <= 400;
-//        ghost3_direction <= `dir_down;
-//
-//        ghost4_x <= 500;
-//        ghost4_y <= 300;
-//        ghost4_direction <= `dir_right;
+//         ghost1_x <= 50;
+//         ghost1_y <= 100;
+//         ghost1_direction <= `dir_up;
 
-//        for(i = 0; i < `tile_row_num; i = i + 1) begin
+//         ghost2_x <= 160;
+//         ghost2_y <= 70;
+//         ghost2_direction <= `dir_left;
+
+//         ghost3_x <= 80;
+//         ghost3_y <= 400;
+//         ghost3_direction <= `dir_down;
+
+//         ghost4_x <= 500;
+//         ghost4_y <= 300;
+//         ghost4_direction <= `dir_right;
+
+//         for(i = 0; i < `tile_row_num; i = i + 1) begin
 //            for(j = 0; j < `tile_col_num; j = j + 1) begin
 //                if(i == `tile_row_num - 1 || j == `tile_col_num - 1) begin
 //                    tilemap_walls[i * `tile_col_num + j] <= 1'b1;
@@ -144,16 +156,13 @@ module PacMan(
 //                end
 //            end
 //        end
-		  
-//		  for(i = 0; i < `tile_row_num; i = i + 1) //test
-//				tilemap_walls[100+i] <= 1'b1;
-//		  for(i = 0; i < `tile_row_num; i = i + 1) //test
-//				tilemap_walls[2*i+32] <= 1'b1;
-		 
+        
+//         for(i = 0; i < `tile_row_num; i = i + 1) //test
+//             tilemap_walls[100+i] <= 1'b1;
+//         for(i = 0; i < `tile_row_num; i = i + 1) //test
+//             tilemap_walls[2*i+32] <= 1'b1;
 //    end
 
-		 
-	
     PlayerControl player_control(
         .clk(clk_100Hz),
         .reset(reset),
@@ -163,55 +172,55 @@ module PacMan(
         .d(d),
         .x(player_x),
         .y(player_y),
-		  .player_direction(player_direction),
-		  .tilemap_walls(tilemap_walls),
-		  .tilemap_dots(tilemap_dots)
+        .player_direction(player_direction),
+        .tilemap_walls(tilemap_walls),
+        .tilemap_dots(tilemap_dots)
     );
-	 
-	 Ghost1Control ghost1_control(
+
+    Ghost1Control ghost1_control(
         .clk(clk_100Hz),
         .reset(reset),
         .x(ghost1_x),
         .y(ghost1_y),
+        .next_x(ghost1_next_x),
+        .next_y(ghost1_next_y),
         .ghost_direction(ghost1_direction),
-		  .player_x(player_x),
-		  .player_y(player_y),
-		  .tilemap_walls(tilemap_walls)
+        .tilemap_walls(tilemap_walls)
     );
-	 
-	 Ghost2Control ghost2_control(
+    
+    Ghost2Control ghost2_control(
         .clk(clk_100Hz),
         .reset(reset),
         .x(ghost2_x),
         .y(ghost2_y),
+        .next_x(ghost2_next_x),
+        .next_y(ghost2_next_y),
         .ghost_direction(ghost2_direction),
-		  .player_x(player_x),
-		  .player_y(player_y),
-		  .tilemap_walls(tilemap_walls)
+        .tilemap_walls(tilemap_walls)
     );
-	 
-	 Ghost3Control ghost3_control(
+
+    Ghost3Control ghost3_control(
         .clk(clk_100Hz),
         .reset(reset),
         .x(ghost3_x),
         .y(ghost3_y),
+        .next_x(ghost3_next_x),
+        .next_y(ghost3_next_y),
         .ghost_direction(ghost3_direction),
-		  .player_x(player_x),
-		  .player_y(player_y),
-		  .tilemap_walls(tilemap_walls)
+        .tilemap_walls(tilemap_walls)
     );
-	
-	 Ghost4Control ghost4_control(
+
+    Ghost4Control ghost4_control(
         .clk(clk_100Hz),
         .reset(reset),
         .x(ghost4_x),
         .y(ghost4_y),
+        .next_x(ghost4_next_x),
+        .next_y(ghost4_next_y),
         .ghost_direction(ghost4_direction),
-		  .player_x(player_x),
-		  .player_y(player_y),
-		  .tilemap_walls(tilemap_walls)
+        .tilemap_walls(tilemap_walls)
     );
-	
+
 
     Renderer renderer(
         .toDisplay(hstate == `DisplayState && vstate == `DisplayState),
@@ -224,14 +233,14 @@ module PacMan(
         .tilemap_big_dots(tilemap_big_dots),
         .player_x(player_x),
         .player_y(player_y),
-        .ghost1_x(ghost1_x),
-        .ghost1_y(ghost1_y),
-        .ghost2_x(ghost2_x),
-        .ghost2_y(ghost2_y),
-        .ghost3_x(ghost3_x),
-        .ghost3_y(ghost3_y),
-        .ghost4_x(ghost4_x),
-        .ghost4_y(ghost4_y),
+        .ghost1_x(ghost1_next_x),
+        .ghost1_y(ghost1_next_y),
+        .ghost2_x(ghost2_next_x),
+        .ghost2_y(ghost2_next_y),
+        .ghost3_x(ghost3_next_x),
+        .ghost3_y(ghost3_next_y),
+        .ghost4_x(ghost4_next_x),
+        .ghost4_y(ghost4_next_y),
         .player_direction(player_direction),
         .ghost1_direction(ghost1_direction),
         .ghost2_direction(ghost2_direction),
@@ -245,20 +254,19 @@ module PacMan(
 
 // add a temp variable to store is start, otherwise standby
     reg isStart;
-    always @(w, a, s, d)
-    begin
-        if(game_state == `GAME_STATE_STANDBY)
-            isStart <= 1;
-		else;
-    end
+    // always @(w, a, s, d)
+    // begin
+    //     if(game_state == `GAME_STATE_STANDBY)
+    //         isStart <= 1;
+    //     else;
+    // end
 
-    always @(posedge clk_100Hz or negedge reset)
+    always @(posedge clk_25MHz or negedge reset)
     begin
-        if(~reset)
-        begin
+        if(!reset)  begin
             // TODO fix
-//            $readmemb("./maps/dots.txt",tilemap_dots);
-//            $readmemb("./maps/big_dots.txt",tilemap_big_dots);
+            // $readmemb("./maps/dots.txt",tilemap_dots);
+            // $readmemb("./maps/big_dots.txt",tilemap_big_dots);
             score <= 0;
             ateDots <= 0;
             game_state <= `GAME_STATE_STANDBY;
@@ -271,10 +279,21 @@ module PacMan(
             ghost4_x <= `GHOST_SPAWN_POINT_X;
             ghost4_y <= `GHOST_SPAWN_POINT_Y;
             power_count_down <= 0;
+            // tilemap_walls <= init_walls;
+            // tilemap_dots <= init_dots;
+            // tilemap_big_dots <= init_big_dots;
         end
-        else
-        begin
-            if(isStart == 1'b1 && game_state == `GAME_STATE_STANDBY)
+        else begin
+            if (isStart == 1'b0 && game_state == `GAME_STATE_STANDBY)
+                if((!w || !a || !s || !d)) begin
+                    isStart <= 1'b1;
+                    game_state <= `GAME_STATE_PLAYING;
+                end
+                else begin
+                    isStart <= 1'b0;
+                    game_state <= `GAME_STATE_STANDBY;
+                end
+            else if(isStart == 1'b1 && game_state == `GAME_STATE_STANDBY)
             begin
                 game_state <= `GAME_STATE_PLAYING;
                 isStart <= 1'b0;
@@ -301,24 +320,24 @@ module PacMan(
             end
             else
             begin
-                    ghost1_x <= ghost1_next_x;
-                    ghost1_y <= ghost1_next_y;
+                ghost1_x <= ghost1_next_x;
+                ghost1_y <= ghost1_next_y;
             end
             if((ghost2_x - player_x) < `tile_size && ((ghost2_x - player_x) > -1*`tile_size) && (ghost2_y - player_y) < `tile_size && ((ghost2_y - player_y) > -1*`tile_size))
             begin
                 if(game_state == `GAME_STATE_PLAYING_POWER)
                 begin
                     score <= score + `GHOST_POINTS;
-                   ghost2_x <= `GHOST_SPAWN_POINT_X;
-                   ghost2_y <= `GHOST_SPAWN_POINT_Y;
+                    ghost2_x <= `GHOST_SPAWN_POINT_X;
+                    ghost2_y <= `GHOST_SPAWN_POINT_Y;
                 end
                 else
                     game_state <= `GAME_STATE_GAMEOVER;
             end
             else
             begin
-                    ghost2_x <= ghost2_next_x;
-                    ghost2_y <= ghost2_next_y;
+                ghost2_x <= ghost2_next_x;
+                ghost2_y <= ghost2_next_y;
             end
             if((ghost3_x - player_x) < `tile_size && ((ghost3_x - player_x) > -1*`tile_size) && (ghost3_y - player_y) < `tile_size && ((ghost3_y - player_y) > -1*`tile_size))
             begin
@@ -333,10 +352,10 @@ module PacMan(
             end
             else 
             begin
-                    ghost3_x <= ghost3_next_x;
-                    ghost3_y <= ghost3_next_y;
+                ghost3_x <= ghost3_next_x;
+                ghost3_y <= ghost3_next_y;
             end
-            if((ghost4_x - player_x) < `tile_size && ((ghost4_x - player_x) > -1*`tile_size) && (ghost4_y - player_y) < `tile_size && ((ghost4_y - player_y) > -1*`tile_size))
+            if(((ghost4_x - player_x) < `tile_size || ((ghost4_x - player_x) < `tile_size)) && ((ghost4_y - player_y) < `tile_size || ((ghost4_y - player_y) < `tile_size)))
             begin
                 if(game_state == `GAME_STATE_PLAYING_POWER)
                 begin
@@ -349,15 +368,15 @@ module PacMan(
             end
             else
             begin
-                    ghost4_x <= ghost4_next_x;
-                    ghost4_y <= ghost4_next_y;
+                ghost4_x <= ghost4_next_x;
+                ghost4_y <= ghost4_next_y;
             end
 
             if(tilemap_dots[player_x/`tile_size + player_y*`tile_col_num/`tile_size] == 1'b1)
             begin
                 ateDots <= ateDots + 1'b1;
                 score <= score + `DOT_POINTS;
-                tilemap_dots[player_x/`tile_size + player_y*`tile_col_num/`tile_size] <= 1'b0;
+                // tilemap_dots[player_x/`tile_size + player_y*`tile_col_num/`tile_size] <= 1'b0;
             end
             else if(tilemap_big_dots[player_x*`tile_col_num/`tile_size + player_y/`tile_size] == 1'b1)
             begin
@@ -375,14 +394,21 @@ module PacMan(
         end
     end
 
-    wire[3:0] scoreDisplay [5:0];
-    assign scoreDisplay[0] = score % 10;
-    assign scoreDisplay[1] = (score % 100) / 10;
-    assign scoreDisplay[2] = (score % 1000) / 100;
-    assign scoreDisplay[3] = (score % 10000) / 1000;
-    assign scoreDisplay[4] = (score % 100000) / 10000;
-    assign scoreDisplay[5] = (score % 1000000) / 100000;
-
+    reg [3:0] scoreDisplay [0:5];
+    always @(posedge clk_100Hz or negedge reset) begin
+        // scoreDisplay[0] <= 0;
+        // scoreDisplay[1] <= 1;
+        // scoreDisplay[2] <= 2;
+        // scoreDisplay[3] <= 3;
+        // scoreDisplay[4] <= 4;
+        // scoreDisplay[5] <= 5;
+        scoreDisplay[0] <= score % 10;
+        scoreDisplay[1] <= (score % 100) / 10;
+        scoreDisplay[2] <= (score % 1000) / 100;
+        scoreDisplay[3] <= (score % 10000) / 1000;
+        scoreDisplay[4] <= (score % 100000) / 10000;
+        scoreDisplay[5] <= (score % 1000000) / 100000;
+    end
     SevenDisplay seven_display0(.num(scoreDisplay[0]), .out(sevenDisplay[7*0 + 6:7*0]));
     SevenDisplay seven_display1(.num(scoreDisplay[1]), .out(sevenDisplay[7*1 + 6:7*1]));
     SevenDisplay seven_display2(.num(scoreDisplay[2]), .out(sevenDisplay[7*2 + 6:7*2]));
@@ -403,31 +429,31 @@ module FrequencyDivider_25MHz (
     end
 endmodule
 module SevenDisplay(num, out);
-	input[3:0] num;
-	reg[6:0] out;
-	output[6:0] out;
-	
-	always @(num)
-	begin
-		case(num)
-			4'h0: out = 7'b1000000;
-			4'h1: out = 7'b1111001;
-			4'h2: out = 7'b0100100;
-			4'h3: out = 7'b0110000;
-			4'h4: out = 7'b0011001;
-			4'h5: out = 7'b0010010;
-			4'h6: out = 7'b0000010;
-			4'h7: out = 7'b1111000;
-			4'h8: out = 7'b0000000;
-			4'h9: out = 7'b0010000;
-			4'hA: out = 7'b0001000;
-			4'hB: out = 7'b0000011;
-			4'hC: out = 7'b1000110;
-			4'hD: out = 7'b0100001;
-			4'hE: out = 7'b0000110;
-			4'hF: out = 7'b0001110;
-			default: out = 7'b1111111;
-		endcase
-	end
+    input[3:0] num;
+    reg[6:0] out;
+    output[6:0] out;
+
+    always @(num)
+    begin
+        case(num)
+            4'h0: out = 7'b1000000;
+            4'h1: out = 7'b1111001;
+            4'h2: out = 7'b0100100;
+            4'h3: out = 7'b0110000;
+            4'h4: out = 7'b0011001;
+            4'h5: out = 7'b0010010;
+            4'h6: out = 7'b0000010;
+            4'h7: out = 7'b1111000;
+            4'h8: out = 7'b0000000;
+            4'h9: out = 7'b0010000;
+            4'hA: out = 7'b0001000;
+            4'hB: out = 7'b0000011;
+            4'hC: out = 7'b1000110;
+            4'hD: out = 7'b0100001;
+            4'hE: out = 7'b0000110;
+            4'hF: out = 7'b0001110;
+            default: out = 7'b1111111;
+        endcase
+    end
 
 endmodule
--- VGASateMachine.v
+++ VGASateMachine.v
@@ -6,7 +6,6 @@ module VGAStateMachine
 // )
 (
     input clk,
-    input reset,
     output reg [$clog2(`WIDTH) - 1:0] x,
     output reg [$clog2(`HEIGHT) - 1:0] y,
     output reg [1:0] h_state, 
@@ -29,75 +28,64 @@ module VGAStateMachine
     end
 
     always @(negedge clk) begin
-        if(!reset) begin
-            h <= `SyncState;
-            v <= `SyncState;
+        if(counter_x == target_counter_x) begin
             counter_x <= 0;
+            case(h)
+                `SyncState: begin
+                    h <= `BackPorchState;
+                    target_counter_x <= `hsync_back_porch;
+                end
+                `BackPorchState: begin
+                    h <= `DisplayState;
+                    target_counter_x <= `WIDTH;
+                end
+                `DisplayState: begin
+                    h <= `FrontPorchState;
+                    target_counter_x <= `hsync_front_porch;
+                end
+                `FrontPorchState: begin
+                    h <= `SyncState;
+                    target_counter_x <= `hsync_pulse_width;
+                    counter_y <= counter_y + 1'h1;
+                end
+                default: begin
+                    h <= `SyncState;
+                    target_counter_x <= `hsync_pulse_width;
+                end
+            endcase
+        end
+        else counter_x <= counter_x + 1'h1;
+
+        if(counter_y == target_counter_y) begin
             counter_y <= 0;
-            target_counter_x <= `WIDTH;
-            target_counter_y <= `HEIGHT;
+            case(v)
+                `SyncState: begin
+                    v <= `BackPorchState;
+                    target_counter_y <= `vsync_back_porch;
+                end
+                `BackPorchState: begin
+                    v <= `DisplayState;
+                    target_counter_y <= `HEIGHT;
+                end
+                `DisplayState: begin
+                    v <= `FrontPorchState;
+                    target_counter_y <= `vsync_front_porch;
+                end
+                `FrontPorchState: begin
+                    v <= `SyncState;
+                    target_counter_y <= `vsync_pulse_width;
+                end
+                default: begin
+                    v <= `SyncState;
+                    target_counter_y <= `vsync_pulse_width;
+                end
+            endcase
         end
-        else begin
-            if(counter_x == target_counter_x) begin
-                counter_x <= 0;
-                case(h)
-                    `SyncState: begin
-                        h <= `BackPorchState;
-                        target_counter_x <= `hsync_back_porch;
-                    end
-                    `BackPorchState: begin
-                        h <= `DisplayState;
-                        target_counter_x <= `WIDTH;
-                    end
-                    `DisplayState: begin
-                        h <= `FrontPorchState;
-                        target_counter_x <= `hsync_front_porch;
-                    end
-                    `FrontPorchState: begin
-                        h <= `SyncState;
-                        target_counter_x <= `hsync_pulse_width;
-                        counter_y <= counter_y + 1'h1;
-                    end
-                    default: begin
-                        h <= `SyncState;
-                        target_counter_x <= `hsync_pulse_width;
-                    end
-                endcase
-            end
-            else counter_x <= counter_x + 1'h1;
+        else ;
 
-            if(counter_y == target_counter_y) begin
-                counter_y <= 0;
-                case(v)
-                    `SyncState: begin
-                        v <= `BackPorchState;
-                        target_counter_y <= `vsync_back_porch;
-                    end
-                    `BackPorchState: begin
-                        v <= `DisplayState;
-                        target_counter_y <= `HEIGHT;
-                    end
-                    `DisplayState: begin
-                        v <= `FrontPorchState;
-                        target_counter_y <= `vsync_front_porch;
-                    end
-                    `FrontPorchState: begin
-                        v <= `SyncState;
-                        target_counter_y <= `vsync_pulse_width;
-                    end
-                    default: begin
-                        v <= `SyncState;
-                        target_counter_y <= `vsync_pulse_width;
-                    end
-                endcase
-            end
-            else ;
-
-            h_state <= h;
-            v_state <= v;
-            x <= counter_x;
-            y <= counter_y;
-        end
-        
+        h_state <= h;
+        v_state <= v;
+        x <= counter_x;
+        y <= counter_y;
     end
 endmodule
